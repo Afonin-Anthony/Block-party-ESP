ceb8c3a335b409dfcb21d826ac0c3f87faa089f2
Anthony <afoninanthony@gmail.com>
2024-08-10 13:24:39 +0300

    Release 1.0.1

--- src/main/java/com/anon987666/blockpartyesp/BlockPartyESP.java
+++ src/main/java/com/anon987666/blockpartyesp/BlockPartyESP.java
@@ -40,7 +40,7 @@ public class BlockPartyESP {
 
 	public static final String NAME = "Block Party ESP";
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
